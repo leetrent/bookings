58d69693ed912491422fec097cb8b0d57c91b961
LeeTrent <lee.trent.1@gmail.com>
2021-08-28 10:30:58 -0500

    Throws divide by zero error

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -15,6 +16,27 @@ func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the ABOUT page (%d)!", add(2, 3))
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	x := 5.0
+	y := 0.0
+	z, err := divideValues(x, y)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%f divided by %f is %f", x, y, z)
+}
+
+func divideValues(x, y float64) (float64, error) {
+	if y == 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+
+	return x / y, nil
+}
+
 func add(x, y int) int {
 	return x + y
 }
@@ -22,6 +44,7 @@ func add(x, y int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Printf("Starting application on port %s", port)
 	http.ListenAndServe(port, nil)
