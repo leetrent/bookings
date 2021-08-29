9eeb61ab0f3e9e681c0f80b398d223afb959819c
LeeTrent <lee.trent.1@gmail.com>
2021-08-29 08:50:50 -0500

    Split code into 3 files (main.go, handlers.go, render.go)

--- handlers.go
+++ handlers.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 func Home(rw http.ResponseWriter, req *http.Request) {
@@ -12,19 +10,4 @@ func Home(rw http.ResponseWriter, req *http.Request) {
 
 func About(rw http.ResponseWriter, req *http.Request) {
 	renderTemplate(rw, "about.page.tmpl")
-}
-
-func renderTemplate(rw http.ResponseWriter, fileName string) {
-	parsedTemplate, err := template.ParseFiles("./templates/" + fileName)
-	if err != nil {
-		fmt.Printf("error encountered when invoking template.ParseFiles for %s", fileName)
-		return
-	}
-
-	err2 := parsedTemplate.Execute(rw, nil)
-	if err2 != nil {
-		fmt.Printf("error encountered when invoking parsedTemplate.Execute for %s", fileName)
-		return
-	}
-
 }
---
+++ render.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func renderTemplate(rw http.ResponseWriter, fileName string) {
+	parsedTemplate, err := template.ParseFiles("./templates/" + fileName)
+	if err != nil {
+		fmt.Printf("error encountered when invoking template.ParseFiles for %s", fileName)
+		return
+	}
+
+	err2 := parsedTemplate.Execute(rw, nil)
+	if err2 != nil {
+		fmt.Printf("error encountered when invoking parsedTemplate.Execute for %s", fileName)
+		return
+	}
+}
