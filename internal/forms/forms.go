39d24060404b97e4f8863546754d6ed37e52f14b
LeeTrent <lee.trent.1@gmail.com>
2021-10-01 23:13:03 -0500

    Working on make reservation validation

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,10 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type Form struct {
@@ -22,7 +24,6 @@ func (f *Form) Has(field string, r *http.Request) bool {
 	//return r.Form.Get(field) != ""
 	value := r.Form.Get(field)
 	if value == "" {
-		f.Errors.Add(field, "This field cannot be blank.")
 		return false
 	}
 	return true
@@ -31,4 +32,23 @@ func (f *Form) Has(field string, r *http.Request) bool {
 // Valid return true if there are no form errors, otherwise true
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
+}
+
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank.")
+		}
+	}
+}
+
+// MinLength check for minimum string length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	value := r.Form.Get(field)
+	if len(value) < length {
+		f.Errors.Add(field, fmt.Sprintf("%s must be at least %d characters long.", field, length))
+		return false
+	}
+	return true
 }
